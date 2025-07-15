23ae4e313b061a4fc52a9e2264c10ab20e8e7364
KJam <kaytchjam@gmail.com>
2025-07-15 11:08:32 -0500

    simple GraphML serialization function made

--- frontend/src/utility/ExportGraph.ts
+++ frontend/src/utility/ExportGraph.ts
@@ -7,28 +7,32 @@ interface GraphExporter {
 }
 
 export class GraphMLExporter implements GraphExporter {
+    /** Add an XML header for the GraphML format */
+    private add_header() {
+        return `<?xml version="1.0" encoding="UTF-8"?>\n<graphml xmlns="http://graphml.graphdrawing.org/xmlns" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns  http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">\n`;
+    }
   
     /** Add an XML key-node */
     private add_key(id: string, gtarget: string, attr_name: string, attr_type: string): string {
-        return `<key id="${id}" for="${gtarget}" attr.name="${attr_name}" attr.type="${attr_type}">\n`;
+        return `\t<key id="${id}" for="${gtarget}" attr.name="${attr_name}" attr.type="${attr_type}">\n`;
     }
     
     /** Add an XML Graph-node */
     private add_graph(id: string, edgedefault: string): string {
-        return `<graph id="${id}" edgedefault="${edgedefault}">\n`;
+        return `\t<graph id="${id}" edgedefault="${edgedefault}">\n`;
     }
 
     /** Build an XML node-node */
-    private build_node(G: kGraph<MetaNode, string>, node_idx: number): string {
+    private add_node(G: kGraph<MetaNode, string>, node_idx: number): string {
         const weight: MetaNode = G.node_weight(node_idx);
-        return `\t<node id="n${node_idx}">\n\t\t<data key="d0">${weight.name}</data>\n\t</node>\n`;
+        return `\t\t<node id="n${node_idx}">\n\t\t\t<data key="d0">${weight.name}</data>\n\t\t</node>\n`;
     }
     
     /** Build an XML edge-node */
-    private build_edge(G: kGraph<MetaNode, string>, edge_idx: number): string {
+    private add_edge(G: kGraph<MetaNode, string>, edge_idx: number): string {
         const weight: string = G.edge_weight(edge_idx);
         const link = G.edge_nodes(edge_idx);
-        return `\t<edge id="e${edge_idx}" source="n${link.from_node}" target="n${link.to_node}">\n\t\t<data key="d1">${weight}</data>\n\t</edge>\n`;
+        return `\t\t<edge id="e${edge_idx}" source="n${link.from_node}" target="n${link.to_node}">\n\t\t\t<data key="d1">${weight}</data>\n\t\t</edge>\n`;
     }
 
     /** Takes in a kGraph and serializes it into string form */
@@ -36,19 +40,20 @@ export class GraphMLExporter implements GraphExporter {
         const N: number = G.num_nodes();
         const E: number = G.num_edges();
         
-        let data: string = this.add_key("d0", "node", "name", "string");
+        let data: string = this.add_header();
+        data += this.add_key("d0", "node", "name", "string");
         data += this.add_key("d1", "edge", "e-text", "string");
         data += this.add_graph("G", "directed");
 
         for (let i = 0; i < N; i++) {
-            data += this.build_node(G, i);
+            data += this.add_node(G, i);
         }
         
         for (let e = 0; e < E; e++) {
-            data += this.build_edge(G, e);
+            data += this.add_edge(G, e);
         }
 
-        data += "</graph>\n";
+        data += "\t</graph>\n</graphml>";
         return data;
     }
 }
