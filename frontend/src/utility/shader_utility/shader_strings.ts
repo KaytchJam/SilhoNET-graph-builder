a8af7579296575116f2eb72b2d653684882aa04c
KJam <kaytchjam@gmail.com>
2025-07-16 15:55:03 -0500

    added some comments to the shader strings

--- frontend/src/utility/shader_utility/shader_strings.ts
+++ frontend/src/utility/shader_utility/shader_strings.ts
@@ -1,6 +1,7 @@
 
 const glsl = (x: any) => x;
 
+/** Vertex shader text for texture plane used in canvas background */
 export const texture_plane_vs_text = glsl`
     attribute vec4 aVertexPosition;
     attribute vec2 aTexCoord;
@@ -12,7 +13,8 @@ export const texture_plane_vs_text = glsl`
         texCoord = aTexCoord;
     }
 `;
-        
+
+/** Fragment shader text for texture plane used in canvas background */
 export const texture_plane_fs_text = glsl`
     precision mediump float;
     varying lowp vec2 texCoord;
@@ -21,11 +23,12 @@ export const texture_plane_fs_text = glsl`
     
     void main() {
         vec4 color = texture2D(uSampler, texCoord);
-        vec4 white = vec4(1.0);
+        vec4 white = vec4(1.0); // make the texture slightly white so it's easier to see the graph nodes
         gl_FragColor = mix(color, white, 0.4);
     }
 `;
 
+/** Vertex shader text for the Graph nodes on the canvas */
 export const render_graph_vs_text = glsl`
     attribute vec4 aVertexPosition;
 
@@ -36,14 +39,40 @@ export const render_graph_vs_text = glsl`
         float nx = aVertexPosition.x / width * 2.0 - 1.0;
         float ny = (height - aVertexPosition.y) / height * 2.0 - 1.0;
 
+        gl_Position = vec4(nx, ny, 0.0, 1.0);
+        gl_PointSize = 10.0;
+    }
+`;
+
+/** Fragment shader text for the Graph nodes on the canvas */
+export const render_graph_fs_text = glsl`
+    void main() {
+        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+    }
+`;
+
+/** Vertex Shader text for the user's cursor on the screen. */
+export const user_mouse_vs_text = glsl`
+    precision lowp float;
+    attribute vec4 aVertexPosition;
+    
+    void main() {
+        const float width = 600.0;
+        const float height = 400.0;
+
+        float nx = aVertexPosition.x / width * 2.0 - 1.0;
+        float ny = (height - aVertexPosition.y) / height * 2.0 - 1.0; // flip axis
 
         gl_Position = vec4(nx, ny, 0.0, 1.0);
         gl_PointSize = 10.0;
     }
 `;
 
-export const render_graph_fs_text = glsl`
+/** Fragment Shader text for the user's cursor on the screen. */
+export const user_mouse_fs_text = glsl`
+    precision lowp float;
+
     void main() {
-        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+        gl_FragColor = vec4(0.0, 0.0, 0.0, 0.5);
     }
 `;
