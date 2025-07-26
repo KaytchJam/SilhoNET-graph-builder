4ffed94294deaceee74da52bd76280bbc2851d08
KJam <kaytchjam@gmail.com>
2025-07-26 02:56:19 -0500

    decoupled node attributes from the nodes themselves. They're now stored in a seperate data strucutre, being a Map<string,string[]> where any node's attributes can be accessed through the use of their node index

--- frontend/src/components/WebGLComponentTwo.tsx
+++ frontend/src/components/WebGLComponentTwo.tsx
@@ -2,9 +2,9 @@ import React from "react"
 import { vec2 } from "gl-matrix";
 import { TexturePlane } from "../utility/TexturePlane";
 import { kGraph, type node_idx_t } from "../utility/graph_utility/KGraph";
-import { RenderGraph, metanode_build, type MetaNode } from "../utility/RenderGraph";
+import { RenderGraph, drawnode_build, type DrawNode } from "../utility/RenderGraph";
 import { ClickEnum, get_lc_states, type TClickEnum } from "../utility/UIStates";
-import { type GraphExporter, GraphMLExporter, DOTExporter } from "../utility/ExportGraph";
+import { type MetaGraphExporter, GraphMLExporter, DOTExporter } from "../utility/ExportGraph";
 import { MouseTracker } from "../utility/MouseTracker";
 
 // Convert "image" File types into HTMLImageElements
@@ -55,7 +55,7 @@ function select(pos: Readonly<vec2>, rg: RenderGraph, thresh: number = 5.0): nod
     let match: number = -1;
     let match_dist_sqrd: number = Number.POSITIVE_INFINITY;
 
-    rg.peek((g: Readonly<kGraph<MetaNode, string>>): void => {
+    rg.peek((g: Readonly<kGraph<DrawNode, string>>): void => {
         for (let i = 0; i < g.num_nodes(); i++) {
             const node_pos: vec2 = g.node_weight(i).position;
             const dist_sqrd: number = vec2.sqrDist(pos, node_pos);
@@ -75,16 +75,49 @@ function select(pos: Readonly<vec2>, rg: RenderGraph, thresh: number = 5.0): nod
 //     left_click: boolean;
 // }
 
-function MetadataCard() {
+// basic bare bone necessities of the Metacard (witholding adding attributes)
+
+// #1 knowledge of the "SelectedNode" state
+// -> is a node currently being selected
+// -> WHICH node is currently being selected
+
+// #2 ability to update the graph
+// -> pass in a "node_weight_update" function to the component
+// -> node_weight_update(n: node_idx_t, weight: N) where n is the node we want to update and weight is the new weight
+
+type MetaCardInput = {
+    selected_node: number | null;
+    metadata_map: Map<string,string[]>
+}
+
+/** React componenet for rendering 'node' cards or 'key' cards
+ *
+ * 
+ *  -> Key Cards: Add or remove node attributes
+ *  -> Node cards: Edit a selected node's particular attributes
+ */
+function MetadataCard(meta_in: MetaCardInput) {
     const update_metadata = (n: node_idx_t) => { n };
-    const node_index: node_idx_t = 0;
+    const node_index: node_idx_t = meta_in.selected_node!;
 
    return (
-        <div>
-            <h4> Node Metadata </h4>
+        <div className="meta-card-div">
+            <h4>Node Metadata</h4>
             <label>Name: <input type="text" value="" name="node-name-input" onChange={() => { update_metadata(node_index) }}/></label>
         </div>
    );
+}
+
+function init_metadata_map() {
+    const metamap = new Map<string,string[]>();
+    metamap.set("name", []);
+    // metamap.set("")
+    return metamap;
+}
+
+function append_to_all(metadata_map: Map<string,string[]>) {
+    for (let [_, value] of metadata_map) { value.push(""); }
+    return metadata_map;
 }
 
 function WebGLCanvas(bg: { image_input: HTMLImageElement }): React.JSX.Element {
@@ -93,6 +126,7 @@ function WebGLCanvas(bg: { image_input: HTMLImageElement }): React.JSX.Element {
 
     const mouse_pos = React.useRef<vec2>([0, 0]);
     const rgraph = React.useRef<RenderGraph>(null);
+    const mmap = React.useRef(init_metadata_map());
     const left_click = React.useRef<boolean>(false);
     const selected_node = React.useRef<node_idx_t>(null);
     const [exportFormat, setExportFormat] = React.useState<"graphml" | "gv">("graphml");
@@ -102,11 +136,12 @@ function WebGLCanvas(bg: { image_input: HTMLImageElement }): React.JSX.Element {
 
     const handleExport = () => {
         const rg: RenderGraph | null = rgraph.current;
+        const mm: Map<string,string[]> = mmap.current;
         if (!rg) return;
 
-        const graph: kGraph<MetaNode,string> = rg.expose_graph();
+        const graph: kGraph<DrawNode,string> = rg.expose_graph();
 
-        let exporter: GraphExporter;
+        let exporter: MetaGraphExporter;
         switch (exportFormat) {
             case "graphml":
                 exporter = new GraphMLExporter();
@@ -119,7 +154,7 @@ function WebGLCanvas(bg: { image_input: HTMLImageElement }): React.JSX.Element {
                 return;
         }
 
-        const output: string = exporter.serialize(graph); // assumes this returns a string
+        const output: string = exporter.serialize(graph, mm); // assumes this returns a string
         const blob: Blob = new Blob([output], { type: "text/plain;charset=utf-8" });
         const url: string = URL.createObjectURL(blob);
 
@@ -187,6 +222,7 @@ function WebGLCanvas(bg: { image_input: HTMLImageElement }): React.JSX.Element {
         const render_loop = (_: number) => {
             // copying all "asyncs" at the start of the render loop
             const rg: RenderGraph = rgraph.current!;
+            const mm: Map<string,string[]> = mmap.current;
             const node_picked: number | null = selected_node.current;
             const m_pos: number[] | Float32Array = mouse_pos.current.slice();
             const l_clicked: boolean = left_click.current;
@@ -201,17 +237,19 @@ function WebGLCanvas(bg: { image_input: HTMLImageElement }): React.JSX.Element {
 
                 switch (lc_state) {
                     case ClickEnum.LC_ADD_NODE:
-                        rg.update((g: kGraph<MetaNode, string>): kGraph<MetaNode,string> => {
-                            const m_new: MetaNode = metanode_build(m_pos[0], m_pos[1], "");
+                        rg.update((g: kGraph<DrawNode, string>): kGraph<DrawNode,string> => {
+                            const m_new: DrawNode = drawnode_build(m_pos[0], m_pos[1]);
                             g.add_node(m_new);
                             return g;
                         });
+
+                        mmap.current = append_to_all(mm);
                         selected_node.current = null;
                         break;
 
                     case ClickEnum.LC_ADD_EDGE:
                         if (!rg.expose_graph().has_outgoing_to(node_picked!, hover_node!)) {
-                            rg.update((g: kGraph<MetaNode,string>): kGraph<MetaNode,string> => {
+                            rg.update((g: kGraph<DrawNode,string>): kGraph<DrawNode,string> => {
                                 g.add_edge(node_picked!, hover_node!, "");
                                 return g;
                             });
@@ -248,6 +286,7 @@ function WebGLCanvas(bg: { image_input: HTMLImageElement }): React.JSX.Element {
             <p>Another canvas, with a texture now.</p>
             <canvas ref={canvas_ref} height="400" width="600"></canvas>
             <p ref={para_ref}></p>
+            <MetadataCard selected_node={selected_node.current} metadata_map={mmap.current}/>
             <div>
                 <label>Export format: </label>
                 <select value={exportFormat} onChange={(e) => setExportFormat(e.target.value as any)}>
@@ -261,9 +300,9 @@ function WebGLCanvas(bg: { image_input: HTMLImageElement }): React.JSX.Element {
 }
 
 // TODO:
-// NEED... A CANVAS
-// NEED... TO BE ABLE TO PUT TEXTURES ON THE CANVAS
-// NEED... TO BE ABLE TO "DRAW" POINTS AND LINES ON THE CANVAS -> CONSTRUCT GRAPH
+// NEED... A CANVAS -> DONE
+// NEED... TO BE ABLE TO PUT TEXTURES ON THE CANVAS -> DONE
+// NEED... TO BE ABLE TO "DRAW" POINTS AND LINES ON THE CANVAS -> CONSTRUCT GRAPH -> DONE
 
 export default function WebGLComponentTwo(): React.JSX.Element {
     const [image_in, set_image_in] = React.useState<HTMLImageElement | null>(null);
