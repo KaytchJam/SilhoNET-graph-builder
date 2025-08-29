3792fdacb24b7bfb6c02451e93c4586c39d13409
KJam <kaytchjam@gmail.com>
2025-08-29 09:54:19 -0500

    come comments added. the add function was also added to ImplPositionable

--- frontend/src/utils/types/Positionable.ts
+++ frontend/src/utils/types/Positionable.ts
@@ -1,3 +1,7 @@
+/** Interface for any 2D "Point-like" type.
+ * Maybe I should renamed it to "Positionable2D".
+ * Implementing this gives you access to all the
+ * functions in `ImplPositionable`. */
 export interface Positionable {
     get_x(): number;
     get_y(): number;
@@ -10,16 +14,25 @@ export interface Positionable {
 
 /** Functions available to all classes that implement `Positionable` */
 export const ImplPositionable = {
-
+    /** Calculates the euclidean distance between Positionable p and q */
     distance<P extends Positionable>(p: P, q: P): number {
         const [x1, y1] = p.get_xy();
         const [x2, y2] = q.get_xy();
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     },
 
+    /** Calculates the squared euclidean distance between Positionable p and q */
     distance_squared<P extends Positionable>(p: P, q: P): number {
         const [x1, y1] = p.get_xy();
         const [x2, y2] = q.get_xy();
         return Math.hypot(x2 - x1, y2 - y1);
+    },
+    
+    /** Adds two positionables together and returns the destination to `dest`. If no destination
+     * is provided, then `Positionable p` (the first argument) is used as the destination. */
+    add<P extends Positionable>(p: P, q: P, dest?: P | undefined): P {
+        if (dest === undefined) { dest = p; }
+        dest.set_xy(p.get_x() + q.get_x(), p.get_y() + q.get_y());
+        return dest;
     }
 }
