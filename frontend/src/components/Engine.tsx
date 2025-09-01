148059d4e926ee6f1e83c406d7dc0d214fed4d65
KJam <kaytchjam@gmail.com>
2025-09-01 09:24:55 -0500

    removed the iterative circle instanced rendering test and replaced with the time based one. Also messing around with hooks

--- frontend/src/components/Engine.tsx
+++ frontend/src/components/Engine.tsx
@@ -27,177 +27,6 @@ function get_hover_id(mouse_pos: Coord2D, positions: number[], radii: number[]):
     return closest_id;
 }
 
-/** Rendering many circles with our circle instance WITH instanced rendering */
-function with_instance_scene(canvas: HTMLCanvasElement): void {
-    const circle_shader_instanced_vs: string = `
-        precision lowp float;
-    
-        attribute vec2 aVertexPosition;
-        attribute vec3 aVertexOffset;
-        attribute float aRadius;
-
-        varying float vCircleID;
-    
-        void main() {
-            const float width = 600.0;
-            const float height = 400.0;
-
-            vCircleID = aVertexOffset.z; // extract the circle ID
-    
-            float sX = aVertexPosition.x * aRadius + aVertexOffset.x;
-            float sY = aVertexPosition.y * aRadius + aVertexOffset.y;
-
-            float nx = sX / width * 2.0 - 1.0;
-            float ny = (height - sY) / height * 2.0 - 1.0;
-    
-            gl_Position = vec4(nx, ny, 0.0, 1.0);
-        }
-    `;
-
-     /** Fragment Shader */
-    const circle_shader_instanced_fs: string = `
-        precision lowp float;
-
-        uniform int uHoverID;
-        varying float vCircleID;
-
-        void main() {
-            vec4 color_out = vec4(0.0, 1.0, 0.0, 1.0);
-            if (uHoverID == int(vCircleID)) {
-                color_out = vec4(1.0, 0.0, 0.0, 1.0);
-            }
-
-            gl_FragColor = color_out;
-        }
-    `;
-
-    const circle_inst: CircleInstance = new CircleInstance(9);
-    const positions: number[] = [
-        100.0, 200.0, 0.0, // index (0-1) = offset, // index (2) = instance index
-        150.0, 50.0, 1.0,
-        350.0, 100.0, 2.0
-    ];
-
-    const radii: number[] = [
-        20.0,
-        15.0,
-        40.0
-    ];
-
-    const gl: WebGL2RenderingContext = canvas.getContext("webgl2")!;
-    const num_instances: number = positions.length / 3;
-    const program: WebGLProgram = init_shader_program(gl, circle_shader_instanced_vs, circle_shader_instanced_fs)!;
-    const vbo: WebGLBuffer = gl.createBuffer();
-
-    gl.useProgram(program);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-    gl.bufferData(gl.ARRAY_BUFFER, circle_inst.data(), gl.STATIC_DRAW);
-    
-    const aVPos = gl.getAttribLocation(program, "aVertexPosition"); 
-
-    gl.vertexAttribPointer(
-        aVPos,
-        2,
-        gl.FLOAT,
-        false,
-        0,
-        0
-    );
-    gl.enableVertexAttribArray(aVPos);
-
-    const offset_buffer: WebGLBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, offset_buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-    const aVOff = gl.getAttribLocation(program, "aVertexOffset");
-
-    gl.vertexAttribPointer(
-        aVOff,
-        3,
-        gl.FLOAT,
-        false,
-        0,
-        0
-    );
-
-    gl.enableVertexAttribArray(aVOff);
-    gl.vertexAttribDivisor(aVOff, 1);
-
-    const radii_buffer: WebGLBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, radii_buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(radii), gl.STATIC_DRAW);
-
-    const aRad = gl.getAttribLocation(program, "aRadius");
-
-    gl.vertexAttribPointer(
-        aRad,
-        1,
-        gl.FLOAT,
-        false,
-        0,
-        0
-    );
-
-    gl.enableVertexAttribArray(aRad);
-    gl.vertexAttribDivisor(aRad, 1);
-
-    let uHPos = gl.getUniformLocation(program, "uHoverID");
-    if (uHPos === null) {
-        console.log("Couldn't find the position of uniform `uHoverID`");
-    }
-
-    const mouse_pos: Coord2D = new Coord2D();
-    (gl.canvas as HTMLCanvasElement).addEventListener("mousemove", (event: MouseEvent) => {
-        const bb: DOMRect = (gl.canvas as HTMLCanvasElement).getBoundingClientRect();
-        mouse_pos.set_xy(
-            event.clientX - bb.left,
-            event.clientY - bb.top
-        );
-    });
-
-    // Radius update data
-    const radii_initials: number[] = radii.slice();
-    const MI: number = 10; // max growth we allow in circle size
-    const dg: number = 0.2; // change in growth per frame
-
-    let last_time: number = 0;
-
-    const update = (time: number) => {
-        const dt = (time - last_time) / 16.67;
-        last_time = time;
-
-        const mouse_pos_local: Coord2D = Coord2D.from_coord(mouse_pos);
-        const id = ((id_in: number | null) => { return !(id_in === null || id_in === -1) ? id_in : -1 })(get_hover_id(mouse_pos_local, positions, radii));
-
-        for (let i = 0; i < radii.length; i++) {
-            radii[i] = (i === id) 
-                ? Math.min(radii_initials[i] + MI, radii[i] + dg * dt)
-                : Math.max(radii_initials[i], radii[i] - dg * dt);
-        }
-        
-        gl.bindBuffer(gl.ARRAY_BUFFER, radii_buffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(radii), gl.STATIC_DRAW); // send current circle radius data
-
-        gl.uniform1i(uHPos, id); // send data on the current hovered circle
-    }
-    
-    const render = (time: number) => {
-        update(time);
-
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
-        gl.enable(gl.DEPTH_TEST);
-        gl.clearDepth(1.0);
-        gl.depthFunc(gl.LEQUAL);
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-        gl.drawArraysInstanced(gl.TRIANGLE_FAN, 0, circle_inst.num_points(), num_instances);
-        requestAnimationFrame(render);
-    };
-
-    requestAnimationFrame(render);
-}
-
 function clamp(lower: number, value: number, upper: number) {
     return Math.min(upper, Math.max(lower, value));
 }
@@ -414,9 +243,38 @@ function time_based_with_instance_scene(canvas: HTMLCanvasElement) {
     requestAnimationFrame(render);
 }
 
+function useCustom() {
+    const [value, setValue] = React.useState('');
+
+    React.useEffect(() => {
+        setValue('updated');
+    }, []);
+
+    return value;
+}
+
+function useCanvasRef() {
+    let ref = React.useRef<HTMLCanvasElement>(null);
+
+    React.useEffect(() => {
+        // does work here when the reference is changed?
+    }, []);
+
+    return ref;
+}
+
+function CanvasUserComponent() {
+    let canvas_ref = useCanvasRef();
+    return (
+        <canvas ref={canvas_ref}></canvas>
+    );
+}
 
 export function EnginePage(): React.JSX.Element {
     let ref = React.useRef<HTMLCanvasElement>(null);
+    let val = useCustom();
+
+    console.log(`hook value: ${val}`);
 
     React.useEffect(() => {
         time_based_with_instance_scene(ref.current!);
