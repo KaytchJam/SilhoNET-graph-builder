5d878c6d6253532368d442dc65e83d2f949fb61e
KJam <kaytchjam@gmail.com>
2025-09-09 13:50:49 -0500

    deleted a line

--- frontend/src/libs/engine/gui/Line.ts
+++ frontend/src/libs/engine/gui/Line.ts
@@ -1,10 +1,3 @@
-
-
-// class EdgeVertexBuilder {
-//     vertex_data: Float32Array;
-//     constructor()
-// }
-
 export class LineInstance {
     private vertex_data: Float32Array; // [pointBL(x,y), pointTL(x,y), pointBR(x,y), pointTR(x,y)
     public constructor() { 
@@ -15,6 +8,7 @@ export class LineInstance {
             0.0, 0.0, 3.0
         ]);
     }
+    
     public data(): Float32Array { return this.vertex_data; }
     public num_points(): number { return 4; }
     public vertex_size(): number { return 3; }
