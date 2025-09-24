57c0a3412f5c54c0c1ba306c9659194f31c002a9
KJam <kaytchjam@gmail.com>
2025-09-24 11:29:38 -0500

    added states for moving a node: LC_MOVE_NODE, and LC_MOVE_NODE_START, along with additional logic for checking when to return those states

--- frontend/src/libs/engine/UIStates.ts
+++ frontend/src/libs/engine/UIStates.ts
@@ -7,25 +7,35 @@ export const ClickEnum = {
     LC_SELECT_NODE: 3, 
     LC_DESELECT_NODE: 4,
     LC_MOVE_NODE: 5,
+    LC_MOVE_NODE_START: 6
 } as const;
 
 export type TClickEnum = EnumLike<typeof ClickEnum>;
 
 /** Given an object indicating the node_picked and hover_node status, returns various left click enum states */
-export function get_lc_states(selection_context: { node_picked: number | null, hover_node: number | null}): TClickEnum {
+export function get_lc_states(selection_context: { node_picked: number | null, hover_node: number | null, prev_lc: boolean, prev_state: TClickEnum}): TClickEnum {
     const has_hovering: boolean = selection_context.hover_node !== null;
     const has_picked: boolean = selection_context.node_picked !== null;
     const same_node: boolean = has_hovering && has_picked && (selection_context.node_picked! === selection_context.hover_node!);
+    const click_held: boolean = selection_context.prev_lc;
+    const can_move: boolean = selection_context.prev_state == ClickEnum.LC_MOVE_NODE_START || selection_context.prev_state == ClickEnum.LC_MOVE_NODE;
 
     let out: TClickEnum = ClickEnum.NONE;
-    if (has_hovering && has_picked && !same_node) {
-        out = ClickEnum.LC_ADD_EDGE;
-    } else if (has_hovering && !has_picked) {
-        out = ClickEnum.LC_SELECT_NODE;
-    } else if (!has_hovering && has_picked) {
-        out = ClickEnum.LC_DESELECT_NODE;
-    } else if (!has_hovering && !has_picked) {
-        out = ClickEnum.LC_ADD_NODE;
+    
+    if (click_held && can_move) {
+        out = ClickEnum.LC_MOVE_NODE;
+    } else {
+        if (has_hovering && has_picked && !same_node) {
+            out = ClickEnum.LC_ADD_EDGE;
+        } else if (has_hovering && has_picked && same_node) {
+            out = ClickEnum.LC_MOVE_NODE_START;
+        } else if (has_hovering && !has_picked) {
+            out = ClickEnum.LC_SELECT_NODE;
+        } else if (!has_hovering && has_picked) {
+            out = ClickEnum.LC_DESELECT_NODE;
+        } else if (!has_hovering && !has_picked) {
+            out = ClickEnum.LC_ADD_NODE;
+        }
     }
 
     return out;
